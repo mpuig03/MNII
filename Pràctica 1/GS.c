ac5b21734652659f3f4424e3e65dc2c6709e48ab
mpuig03 <67662270+mpuig03@users.noreply.github.com>
2023-10-26 12:21:11 +0200

    Add files via upload

---
+++ Pràctica 1/GS.c
@@ -0,0 +1,47 @@
+/*MARC PUIG CREIXELL.   NIUB:20576474*/
+/*Aquesta funció serveix per aplicar el mètode de Gauss-Seidel per a resolució
+de sistemes en el cas d'una matriu tridiagonal de diagional A[i][i]=[a1,...,an] i 
+diagonals secundaries fixes A[i][i-1]=A[i][i+1]=b i terme independent v=[v1,...,vn]*/
+
+#include<stdio.h>
+#include<stdlib.h>
+int GS(int n, double tol, double *a, double b, double *v, double *x){
+    double d0, d1=0,e,*x0;
+    int i;
+
+    x0=(double*)malloc(n*sizeof(double));
+    if(x0==NULL){
+        printf("Error de reserva de memòria.\n");
+        return -1;
+    }
+    do{
+        for(i=0;i<n;i++){
+            x0[i]=x[i];
+        }
+
+        /*Iteració Gauss Seidel*/
+        /*Tenim que per components, es simplifiquen molts calculs per la forma de la matriu,
+        així doncs, ens queda el següent*/
+
+        x[0]=(1/a[0])*(v[0]-b*x[1]);
+
+        for(i=1;i<n-1;i++){
+            x[i]=(1/a[i])*(v[i]-b*x[i-1]-b*x0[i+1]);
+        }
+
+        x[n-1]=(1/a[n-1])*(v[n-1]-b*x[n-2]);
+
+        /*Càlcul aproximació fita superior error*/
+        d0=d1;
+        d1=0;
+        for(i=0;i<n;i++){
+            d1+=(x[i]-x0[i])*(x[i]-x0[i]);
+        }
+        d1=sqrt(d1);
+        e=d1*d1/(d0-d1);
+
+    }while(fabs(e)>=tol); 
+
+    free(x0);
+    return 0;
+}
---
+++ Pràctica 1/main.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+#include "potencia.c"
+#include "potencia_inversa.c"
+
+int main(void){
+    double L=5, V0=5, dx;
+    double tol=1e-12;
+    double *V, b;
+    double *z, *aux, *y, smax, smin;
+    int n=50, i;
+
+    /*Reserva de memòria dinàmica*/
+    V=(double*)malloc(n*sizeof(double));
+    z=(double*)malloc(n*sizeof(double));
+    aux=(double*)malloc(n*sizeof(double));
+    y=(double*)malloc(n*sizeof(double));
+    if(V==NULL||z==NULL||aux==NULL||y==NULL){
+        printf("Error de reserva de memòria.\n");
+        return -1;
+    }
+
+    /*ASSIGNACIÓ DE VALORS A LA MATRIU*/
+    dx=2*L/n;
+    /*Posem V a la diagonal, (Vi + 1/dx²)*/
+    for(i=0;i<n;i++){
+        V[i]=0.5*(-L+(i+1)*dx)*(-L+(i+1)*dx)+V0; /*Vi*/
+        V[i]+=1/(dx*dx); /*+1/dx²*/
+    }
+    /*Posem el valor de les diagonals secundàries*/
+    b=-1/(2*dx*dx);
+
+    /*MÈTODE DE LA POTÈNCIA*/
+    z[0]=1;
+    for(i=1;i<n;i++){
+        z[i]=0;
+    }
+    smax=potencia(n,tol,V,b,z,y,aux);
+    printf("VAP màxim: %22.15e\n",smax);
+    /*MÈTODE DE LA POTÈNCIA INVERSA*/
+    z[0]=1;
+    for(i=1;i<n;i++){
+        z[i]=0;
+    }
+    smin=inv_pot(n,tol,V,b,z,y,aux);
+    printf("VAP mínim: %22.15e\n",smin);
+
+
+    free(z);
+    free(V);
+    free(y);
+    free(aux);
+    return 0;  
+}
